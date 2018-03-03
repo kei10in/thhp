e12671fb593625ad317a439ab8d55b12954ee465
kei10in <kei10in@gmail.com>
2018-03-03 18:19:57 +0900

    Parse http header fileds

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,11 @@ pub struct Request<'buffer> {
     pub version: &'buffer [u8],
 }
 
+pub struct HeaderField<'buffer> {
+    pub name: &'buffer [u8],
+    pub value: &'buffer [u8],
+}
+
 pub fn parse_request<'buffer>(buf: &'buffer [u8]) -> Option<Request<'buffer>> {
     if buf.is_empty() {
         return None;
@@ -46,6 +51,50 @@ pub fn parse_request<'buffer>(buf: &'buffer [u8]) -> Option<Request<'buffer>> {
     });
 }
 
+pub fn parse_headers<'buffer>(buf: &'buffer [u8]) -> Vec<HeaderField<'buffer>> {
+    let mut result = Vec::<HeaderField<'buffer>>::new();
+
+    if buf.is_empty() {
+        return result;
+    }
+
+    let mut i = 0;
+
+    loop {
+        if buf[i] == b'\r' {
+            break;
+        }
+
+        let mut s = i;
+
+        while buf[i] != b':' {
+            i += 1;
+        }
+        let name_index = s..i;
+
+        i += 1;
+        s = i;
+
+        while buf[i] != b'\r' {
+            i += 1;
+        }
+        let value_index = s..i;
+
+        i += 1;
+
+        if buf[i] == b'\n' {
+            i += 1;
+        }
+
+        result.push(HeaderField {
+            name: &buf[name_index],
+            value: &buf[value_index],
+        });
+    }
+
+    return result;
+}
+
 #[cfg(test)]
 mod tests {
     use ::*;
@@ -75,5 +124,23 @@ mod tests {
         assert_eq!(req.method, b"POST");
         assert_eq!(req.target, b"/");
         assert_eq!(req.version, b"1.1");
+    }
+
+    #[test]
+    fn parse_a_header_field() {
+        let result = parse_headers(b"name:value\r\n\r\n");
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0].name, b"name");
+        assert_eq!(result[0].value, b"value");
+    }
+
+    #[test]
+    fn parse_2_header_fields() {
+        let result = parse_headers(b"name1:value1\r\nname2:value2\r\n\r\n");
+        assert_eq!(result.len(), 2);
+        assert_eq!(result[0].name, b"name1");
+        assert_eq!(result[0].value, b"value1");
+        assert_eq!(result[1].name, b"name2");
+        assert_eq!(result[1].value, b"value2");
     }
 }
